ee8c3ccf71a90f6c4548998ec321dcc8451d904e
hashhaam <hashamulhaq068@gmail.com>
2026-05-17 13:03:36 +0500

    Fix vercel files

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,5 @@
 import { app } from "../backend/src/app";
+import type { VercelRequest, VercelResponse } from "@vercel/node";
 
+// Export the Express app as a Vercel serverless function
 export default app;
